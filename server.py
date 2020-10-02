99ae20bd8b1e7a7f5659aa68576f2d2c42e6247c
Ronald <rnor3536@uni.sydney.edu.au>
2020-10-02 11:01:12 +1000

    unified stream

--- server.py
+++ server.py
@@ -1,9 +1,10 @@
 from sklearn.datasets import make_blobs
-from datetime import datetime
 import socket #import socket module
 from time import time
+from threading import Thread
+import sys
 
-def create_data(n_samples, n_features, centers, std):
+def createData(n_samples, n_features, centers, std):
     features, target = make_blobs(n_samples = n_samples,
                                   # two feature variables,
                                   n_features = n_features,
@@ -15,23 +16,34 @@ def create_data(n_samples, n_features, centers, std):
                                   shuffle = True)
     return features, target
 
+def sendMessages(numMsgs, host, port, centers):
+    s = socket.socket()  # Create a socket object
+    s.bind((host, port))  # Bind to the port
+    s.listen()
 
-
-s = socket.socket() #Create a socket object
-host = '192.168.122.121'
-# host = socket.gethostbyname_ex(socket.gethostname()) #Get the local machine name
-print("Host: ", socket.gethostname())
-port = 9999 # Reserve a port for your service
-s.bind((host,port)) #Bind to the port
-
-s.listen() #Wait for the client connection
-while True:
-    c,addr = s.accept() #Establish a connection with the client
-    print("Got connection from"+ str(addr))
-    features, target = create_data(10000, 3, 8, 0.65)
-    t1 = time()
+    c,addr = s.accept()
+    features,_ = createData(numMsgs, 3, centers, 0.65)
     for i in features:
-        message = ' '.join([str(j) for j in i])+';'
+        message = ' '.join([str(j) for j in i]) + ';'
         c.send(message.encode())
-    print('Send {} messages in {} seconds'.format(len(features), time()-t1))
     c.close()
+
+if __name__ == "__main__":
+    numMsgs = int(sys.argv[1])
+    host = sys.argv[2]
+    threads = [None]*100
+    centers,_ = createData(8, 3, 8, 0.65)
+    port = 10000
+    t1 = time()
+    for i in range(len(threads)):
+        threads[i] = Thread(target=sendMessages, args=(int(numMsgs/100), host, port, centers))
+        threads[i].start()
+        port += 1
+
+    for i in threads:
+        i.join()
+
+    print('{} messages sent in {} seconds'.format(numMsgs, time()-t1))
+
+
+
